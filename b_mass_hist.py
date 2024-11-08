6e3cce4391f1acdbd75ca3b3b819c77a828b6dda
ThatAquarel <xia_tianyi@outlook.com>
2024-11-08 16:01:52 -0500

    histogram

--- b_mass_hist.py
+++ b_mass_hist.py
@@ -12,6 +12,7 @@ def main(
     bin_width=2,
     sb_fit_ndeg=32,
     b_fit_ndeg=4,
+    sig_res=10,
 ):
     events = pd.read_csv(inv_mass_file, index_col="event")
     pair_cols = [col for col in events.columns if col.startswith("M")]
@@ -34,6 +35,14 @@ def main(
     fit_sb, fit_b = np.poly1d(sb_coeffs), np.poly1d(b_coeffs)
 
     y_sb, y_b = fit_sb(x), fit_b(x)
+    sig = (y_sb - y_b) / np.sqrt(y_b)
+
+    sig_coeffs = np.polyfit(x, sig, sb_fit_ndeg)
+    fit_sig = np.poly1d(sig_coeffs)
+    x_resample = np.linspace(x.min(), x.max(), len(x) * sig_res)
+
+    sig_resample = fit_sig(x_resample)
+    higgs = x_resample[np.argmax(sig_resample)]
 
     fig, (sb, s) = plt.subplots(2, 1, height_ratios=[2, 1])
     fig.subplots_adjust(hspace=0)
@@ -47,15 +56,17 @@ def main(
         x, y_b, ":", color="red", label=f"$Bkg\\ ({b_fit_ndeg}\\ deg\\ poly\\ fit)$"
     )
     sb.tick_params(axis="x", which="both", bottom=False, labelbottom=False)
-
+    m = "m_{\\gamma\\gamma}"
+    sb.axvline(x=higgs, color="blue", label=f"$Higgs:\\ {m}={higgs:.2f}\\ GeV$")
     sb.set_ylabel(f"$Events\\ /\\ {bin_width}\\ GeV$")
 
     s.scatter(x, (y - y_b) / np.sqrt(y_b), color="black")
-    s.plot(x, (y_sb - y_b) / np.sqrt(y_b), color="red")
+    s.plot(x, sig, color="red")
     s.plot(x, (y_b - y_b) / np.sqrt(y_b), ":", color="red")
+    s.axvline(x=higgs, color="blue")
 
-    s.set_ylabel(f"$Significance$")
-    s.set_xlabel("$m_{\\gamma\\gamma} (GeV)$")
+    s.set_ylabel(f"$Significance\\ (\\sigma)$")
+    s.set_xlabel(f"${m}\\ (GeV)$")
     sb.legend(loc="upper right")
 
     plt.show()
