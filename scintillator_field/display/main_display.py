d851f4e4a776ee6477b00c7bcdb8c2a6025f7564
TheBookwyrms <blackdragon6493@gmail.com>
2025-04-09 22:23:34 -0400

    display_1 works for real data. data from 2025-04-09 run.

--- scintillator_field/display/main_display.py
+++ scintillator_field/display/main_display.py
@@ -4,12 +4,25 @@ from OpenGL.GLU import *
 from scintillator_field.display.display_2.Visualizer.app import App
 #from scintillator_field.display.display_1.window import Window
 
+from scintillator_field.display.display_1.drafts.refactor_display_1.display_main import Display_1
+
 
 def main_display():
 
+<<<<<<< HEAD
     width, height = 1000, 750
     app = App((width, height), "title")
     #app = Window()
     #app.main()
+=======
+    #width, height = 1000, 750
+    #app = App((width, height), "title")
+
+    #display = Display_1()
+    #display.run_window()
+
+    app = Window()
+    app.main()
+>>>>>>> be8cd71 (display_1 works for real data. data from 2025-04-09 run.)
 
 
